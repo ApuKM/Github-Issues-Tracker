8e10b642dd4d3b69183d9337710c9958c8d9a8d1
ApuKM <apukumar180@gmail.com>
2026-03-08 00:24:06 +0600

    Applied priority, status with handleStatus function & used map for labels

--- scripts/home.js
+++ scripts/home.js
@@ -1,8 +1,8 @@
 const btnAll = document.getElementById("btn-all");
 const btnOpen = document.getElementById("btn-open");
 const btnClose = document.getElementById("btn-close");
-const allButons = document.querySelectorAll(".toggle-btn");
 const cardContainer = document.getElementById("card-container");
+const allButons = document.querySelectorAll(".toggle-btn");
 
 function selectCategory(id) {
   // console.log(id)
@@ -15,39 +15,61 @@ function selectCategory(id) {
   });
 }
 
+function handleStatus(status, cardWrapper, priority) {
+  const card = cardWrapper.querySelector(".card");
+  const openStatusImg = card.querySelector(".open-status-img");
+  const closeStatusImg = card.querySelector(".close-status-img");
+  const priorityEl = card.querySelector(".priority");
+  //   console.log(status);
+  //   console.log(closeStatusImg);
+  if (status === "open") {
+    openStatusImg.classList.remove("hidden");
+    card.classList.add("border-t-green-500");
+  } else if (status === "closed") {
+    closeStatusImg.classList.remove("hidden");
+    card.classList.add("border-t-purple-500");
+  }
+
+  if (priority === "high") {
+    priorityEl.classList.add("bg-red-100", "text-red-500");
+  } else if (priority === "medium") {
+    priorityEl.classList.add("bg-yellow-100", "text-yellow-500");
+  } else {
+    priorityEl.classList.add("bg-gray-100", "text-gray-500");
+  }
+}
+
 function displayIssues(dataArr) {
   cardContainer.innerHTML = "";
   dataArr.forEach((data) => {
     const div = document.createElement("div");
     div.innerHTML = `
-            <div class="card card-border border-gray-200 bg-white w-fit shadow-lg">
+            <div class="card card-border border-t-3 border-gray-200 bg-white w-fit shadow-lg">
               <div class="card-body p-4">
                 <div class="flex justify-between items-center">
                   <div>
-                    <img src="./assets/Open-Status.png" alt="" />
-                    <img src="./assets/Closed- Status .png" alt="" />
+                    <img  src="./assets/Open-Status.png" alt="" class="hidden open-status-img" />
+                    <img   src="./assets/Closed-Status.png" alt="" class="hidden close-status-img"  />
                   </div>
-                  <h4 class="bg-red-100 px-4 rounded-lg text-red-500">${data.priority}</h4>
+                  <h4 class=" px-4 rounded-lg priority text-sm font-medium">${data.priority.toUpperCase()}</h4>
                 </div>
-                <h2 class="card-title text-[#1F2937] text-sm font-semibold pt-3">${data.title}</h2>
+                <h2 class="card-title text-[#1F2937] text-sm font-semibold pt-3 line-clamp-1">${data.title}</h2>
                 <p class="text-[#64748B] text-xs line-clamp-2">
                   ${data.description}
                 </p>
                 <div class="flex items-center gap-2 pt-3">
-                  <h4 class="bg-red-100 px-3 rounded-lg text-red-500">${data.labels[0]}</h4>
-                  <h4 class="bg-yellow-100 px-3 rounded-lg text-yellow-500">
-                    ${data.labels[1]}
-                  </h4>
+                  ${data.labels.map((label) => `<h4 class="bg-blue-100 px-3 rounded-lg text-blue-500">${label}</h4>`).join("")}
                 </div>
               </div>
               <hr class="text-gray-200" />
               <div class="p-3">
-                <p class="text-[#64748B] text-xs">${data.assignee}</p>
+                <p class="text-[#64748B] text-xs">${data.assignee ? data.assignee : "unknown_assignee"}</p>
                 <p class="text-[#64748B] text-xs">${data.createdAt.split("T")[0]}</p>
               </div>
             </div>
         `;
-        cardContainer.appendChild(div);
+    handleStatus(data.status, div, data.priority);
+    cardContainer.appendChild(div);
   });
 }
 
